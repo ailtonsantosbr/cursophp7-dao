5a6ab3a84756c27fe494cc8f60f695a2fc0d872b
Ailton Silva Dos Santos <ailton.santosbr@hotmail.com>
2022-07-25 19:55:03 -0300

    metodos de lista!

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once("config.php");
 
-/**
+/** SELECT NA TABELA USUARIOS
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
@@ -10,10 +10,35 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 **/
 
+/** CARREGA APENAS UM USUARIO
 $root = new Usuario();
-
 $root->loadById(3);
 
 echo $root;
+
+**/
+
+// CARREGA UMA LISTA DE USUARIOS
+/**
+$lista = Usuario::getList();
+
+echo json_encode($lista);    
+**/
+
+//CARREGA UMA LISTA DE USUARIOS BUSCANDO PELO LOGIN
+/**
+$search = Usuario::search("ail");
+
+echo json_encode($search);
+**/
+
+//CARREGA O USUARIO USANDO O LOGIN E PASSWORD
+
+$usuario = new Usuario();
+
+$usuario->login("ailton", "reste");
+
+echo $usuario;
+
 
  ?>
